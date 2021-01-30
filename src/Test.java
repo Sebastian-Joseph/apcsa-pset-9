5380558ea36c92172272a3c5161bc69d99280ad6
Sebastian Joseph <theclimax25@gmail.com>
2021-01-30 17:40:18 -0500

    second

--- src/Test.java
+++ src/Test.java
@@ -4,15 +4,8 @@ import java.util.Arrays;
         public static void main(String[] args) {
         ProblemSet9 ps9 = new ProblemSet9();
 
-        int[] iarr= {};
-        boolean b = false;
-        int i = 0;
 
-        String s = "";
-
-       int[] arr = new int[] {1, 2, 2, 2, 3, 4, 5, 6};
-
-        //Exercise 1
+        //g0ne
 
         }
     }
